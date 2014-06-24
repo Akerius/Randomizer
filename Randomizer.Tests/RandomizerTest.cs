abf27024da7c85cc49692f685fe8126734a01e60
Bagdad <ntorgov@gmail.com>
2014-06-24 15:05:27 +0400

    1.0.6.0

---
+++ Randomizer.Tests/RandomizerTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleRandomizer;
+
+namespace SimpleRandomizer.Tests
+{
+	[TestClass]
+	public class RandomizerTest
+	{
+		public SimpleRandomizer.Randomizer _randomizer = new SimpleRandomizer.Randomizer();
+
+		[TestMethod]
+		public void GenerateIntGlobalTest()
+		{
+			var someValue = _randomizer.GenerateInt();
+
+			Assert.IsNotNull(someValue, "Число без параметров не генерируется");
+
+			someValue = _randomizer.GenerateInt(100);
+
+			Assert.IsNotNull(someValue, "Простое число не генерируется");
+
+			someValue = _randomizer.GenerateInt(0, 100);
+
+			Assert.IsNotNull(someValue, "Число в диапозоне не генерируется");
+		}
+
+		[TestMethod]
+		public void GenerateIntArrayTest()
+		{
+			int minValue = 100;
+			int maxValue = 0;
+			bool result = false;
+			for (int i = 0; i < 1000; i++)
+			{
+				var someValue = _randomizer.GenerateInt(0, 100);
+
+				if (someValue < minValue) minValue = someValue;
+				if (someValue > maxValue) maxValue = someValue;
+			}
+			if (minValue >= 0 && minValue < 100)
+			{
+				if (maxValue <= 100 && maxValue > 0)
+				{
+					result = true;
+				}
+			}
+
+			Assert.IsTrue(result, "Ошибка генерации в диапозоне");
+		}
+
+		[TestMethod]
+		public void GenerateStringTest()
+		{
+			var newString = _randomizer.GenerateString(6);
+		
+			Assert.IsTrue((newString.Length==6), "Ошибка формирования случайной строки");
+		}
+	}
+}
---
+++ Randomizer/Randomizer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SimpleRandomizer
+{
+	/// <summary>
+	/// Класс генерирует всякие случайности
+	/// </summary>
+    public class Randomizer
+    {
+		/// <summary>
+		/// Экземпляр класса <see cref="Random"/>
+		/// </summary>
+		readonly Random _generator = new Random();
+
+		/// <summary>
+		/// Генератор псевдо-случайного числа от 0 до 100
+		/// </summary>
+		/// <returns></returns>
+		public int GenerateInt()
+		{
+			return GenerateInt(0, 100);
+		}
+
+		/// <summary>
+		/// Генератор псевдо-случайного числа от 0
+		/// </summary>
+		/// <param name="maxValue">Максимальное значение</param>
+		/// <returns></returns>
+		public int GenerateInt(int maxValue = 100)
+		{
+			return GenerateInt(0, maxValue);
+		}
+
+		/// <summary>
+		/// Генератор псевдо-случайного числа
+		/// </summary>
+		/// <param name="minValue">Минимльное значение</param>
+		/// <param name="maxValue">Максимальное значение</param>
+		/// <returns></returns>
+		public int GenerateInt(int minValue = 0, int maxValue=100)
+		{
+			var result = _generator.Next(minValue, maxValue);
+
+			return result;
+		}
+
+		/// <summary>
+		/// Функция генерирует случайную строку
+		/// </summary>
+		/// <param name="counter">Количество символов</param>
+		/// <returns></returns>
+		public string GenerateString(int counter = 6)
+		{
+			var result = "";
+
+			for (var i = 0; i < counter; i++)
+			{
+				var code = GenerateInt(97, 122);
+				result = result + (char) code;
+			}
+			return result;
+		}
+    }
+}
